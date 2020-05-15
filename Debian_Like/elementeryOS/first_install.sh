8c0094351146e605a38de30bf18e3564e494de14
Tim <tim.lorse@gmx.de>
2020-05-15 21:34:57 +0200

    Update first_install.sh

--- Debian_Like/elementeryOS/first_install.sh
+++ Debian_Like/elementeryOS/first_install.sh
@@ -6,5 +6,5 @@ sudo apt install vlc apt-transport-https curl -y
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-# kodecs installieren
+# Multimedia Codecs installieren
 sudo apt install ubuntu-restricted-extras libavcodec-extra libdvd-pkg preload -y
