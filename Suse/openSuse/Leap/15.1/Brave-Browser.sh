e4f52c8f50592c7f65f680e452f637c748636afc
Tim <tim.lorse@gmx.de>
2020-05-15 21:15:16 +0200

    Added Brave Browser

---
+++ Suse/openSuse/Leap/15.1/Brave-Browser.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo zypper install curl
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
+sudo zypper install brave-browser -y
