0b6e0e148d5a54e718ec68dcbf1ac7e6b9b20bba
Tim <tim.lorse@gmx.de>
2021-05-03 00:23:26 -0700

    Update Ubuntu_Remove.sh

--- Debian_Like/Ubuntu/20.04/Ubuntu_Remove.sh
+++ Debian_Like/Ubuntu/20.04/Ubuntu_Remove.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo apt remove firefox firefox-locale-de firefox-locale-en thunderbird thunderbird-gnome-support thunderbird-locale-de thunderbird-locale-en thunderbird-locale-en-gb thunderbird-locale-en-us -y
+sudo apt remove firefox firefox-locale-de firefox-locale-en \
+thunderbird thunderbird-gnome-support thunderbird-locale-de \
+thunderbird-locale-en thunderbird-locale-en-gb thunderbird-locale-en-us -y
